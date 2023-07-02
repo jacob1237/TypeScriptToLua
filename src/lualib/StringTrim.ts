434a4a451a8d1a421bfa8649b87c197987738b12
Jacob Shtokolov <jacob.100205@gmail.com>
2023-07-02 18:43:35 +0400

    Don't trim BOM in trim() polyfills

--- src/lualib/StringTrim.ts
+++ src/lualib/StringTrim.ts
@@ -1,5 +1,6 @@
 export function __TS__StringTrim(this: string): string {
     // http://lua-users.org/wiki/StringTrim
-    const [result] = string.gsub(this, "^[%s\xA0\uFEFF]*(.-)[%s\xA0\uFEFF]*$", "%1");
+    // const [result] = string.gsub(this, "^[%s\xA0\uFEFF]*(.-)[%s\xA0\uFEFF]*$", "%1");
+    const [result] = string.gsub(this, "^[%s%c]*(.-)[%s%c]*$", "%1");
     return result;
 }
--- src/lualib/StringTrimEnd.ts
+++ src/lualib/StringTrimEnd.ts
@@ -1,5 +1,5 @@
 export function __TS__StringTrimEnd(this: string): string {
     // http://lua-users.org/wiki/StringTrim
-    const [result] = string.gsub(this, "[%s\xA0\uFEFF]*$", "");
+    const [result] = string.gsub(this, "[%s%c]*$", "");
     return result;
 }
